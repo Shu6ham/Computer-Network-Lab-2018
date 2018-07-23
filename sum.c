2afffe17d88d0043cfb7a0baa157719ccdfd5922
Shu6ham <shubhamsinnghal@gmail.com>
2018-07-23 23:38:31 +0530

    Sum of two number using commamnd line argument in c

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	printf("No. of arguments is %d\n",argc);
+	
+	printf("Sum of %s and %s is: %d\n",argv[1],argv[2],atoi(argv[1])+atoi(argv[2]));
+	
+}
