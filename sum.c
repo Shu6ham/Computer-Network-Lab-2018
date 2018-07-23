ecb23d97e24cf5d84d6b3f6b6a1f1fa85970baa5
Shu6ham <shubhamsinnghal@gmail.com>
2018-07-23 23:42:43 +0530

    Sum using command line argument in c

--- sum.c
+++ sum.c
@@ -4,7 +4,7 @@
 int main(int argc,char *argv[])
 {
 	printf("No. of arguments is %d\n",argc);
-	
+	//										           atoi for conversion of string into int
 	printf("Sum of %s and %s is: %d\n",argv[1],argv[2],atoi(argv[1])+atoi(argv[2]));
 	
 }
